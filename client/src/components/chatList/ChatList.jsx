d5b84f082d0a1b038757772f2f8be3a22b72c0f9
vishwajeet-8 <vishwajeetrout8@gmail.com>
2024-09-23 23:14:01 +0530

    log the issues of clerk-react

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import "./chatList.css";
 import { useQuery } from "@tanstack/react-query";
-import { useAuth } from "@clerk/clerk-react";
+// import { useAuth } from "@clerk/clerk-react";
 
-const ChatList = async () => {
-  const { getToken } = useAuth();
-  const token = await getToken();
+const ChatList = () => {
+  // const { getToken } = useAuth();
+  // const token = await getToken();
   const { isPending, error, data } = useQuery({
     queryKey: ["userChats"],
     queryFn: () =>
       fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
-        headers: { Authorization: `Bearer ${token}` },
+        // headers: { Authorization: `Bearer ${token}` },
         credentials: "include",
       }).then((res) => res.json()),
   });

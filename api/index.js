ef0a17c9d5e042aca5faa3615b5fb5dbc60fd8d0
vishwajeet-8 <vishwajeetrout8@gmail.com>
2024-09-23 17:40:08 +0530

    Fixed the CORS issue

--- api/index.js
+++ api/index.js
@@ -12,12 +12,26 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 8080;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ai-chat-frontend-l89t.onrender.com",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
